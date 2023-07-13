158a4c9cad623820c8e8cec6068d1af1545ede95
TeamAbella060420 <teamabella@outlook.com>
2023-07-13 12:36:13 +0800

    Update Footer.jsx

--- car-rental/src/components/Footer/Footer.jsx
+++ car-rental/src/components/Footer/Footer.jsx
@@ -52,15 +52,35 @@ const Footer = () => {
           <Col lg="2" md="4" sm="6">
             <h5 className="footer__link-title">
               <ListGroup>
-              {quickLinks.map((item, index) => (
-                <ListGroupItem  key={index} className="p-0 mt-3">
-                 <Link to={item.path}>
-                 {item.display}
-                 </Link>
-                </ListGroupItem>
-              ))}
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
               </ListGroup>
             </h5>
+          </Col>
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Head Office</h5>
+              <p className="office__info">143 Parañaque City, Philippines</p>
+              <p className="office__info">Phone: +6391234567</p>
+              <p className="office__info">Email: test163@gmail.com</p>
+              <p className="office__info">Office Time: 10am - 7pm</p>
+            </div>
+          </Col>
+          <Col lg='3' md='4'>
+            <div className="mb-4">
+              <h5 className="footer__link-title">
+                Newsletter
+              </h5>
+              <p className="section__description">
+                Subscribe to our newsletter
+              </p>
+              <div className="newsletter">
+                <input type="email" name="" id="" placeholder="Email"/>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
