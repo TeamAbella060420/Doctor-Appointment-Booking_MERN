9d252ee92193f9c0ff17c4f9c207ce074b8e0c04
TeamAbella060420 <teamabella@outlook.com>
2023-07-14 11:59:14 +0800

    Create HeroSlide.jsx

---
+++ car-rental/src/components/UI/HeroSlide.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container } from "reactstrap";
+import Slider from "react-slick";
+import { Link } from 'react-router-dom';
+import '../../styles/hero-slide.css'
+
+const HeroSlide = () => {
+
+    const settings = {
+        fade: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false
+    }
+
+  return (
+    <Slider {...settings} className="hero__slider">
+      <div className="slider__item slider__item-01">
+        <Container>
+          <div className="slider__content">
+            <h4 className="text-light mb3">For Rent Php700 Per Day</h4>
+            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          </div>
+
+          <button className="btn reserve__btn mt-4">
+            <Link to="/cars">Reserve Now</Link>
+          </button>
+        </Container>
+      </div>
+
+      <div className="slider__item slider__item-02">
+        <Container>
+          <div className="slider__content">
+            <h4 className="text-light mb3">For Rent Php700 Per Day</h4>
+            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          </div>
+
+          <button className="btn reserve__btn mt-4">
+            <Link to="/cars">Reserve Now</Link>
+          </button>
+        </Container>
+      </div>
+
+      <div className="slider__item slider__item-03">
+        <Container>
+          <div className="slider__content">
+            <h4 className="text-light mb3">For Rent Php700 Per Day</h4>
+            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+          </div>
+
+          <button className="btn reserve__btn mt-4">
+            <Link to="/cars">Reserve Now</Link>
+          </button>
+        </Container>
+      </div>
+    </Slider>
+  );
+};
+
+export default HeroSlide;
