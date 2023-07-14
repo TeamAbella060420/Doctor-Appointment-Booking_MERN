4b1d0e58b41ead5e73f8775ce1315bf59648a6b5
TeamAbella060420 <teamabella@outlook.com>
2023-07-14 11:59:25 +0800

    Update Home.jsx

--- car-rental/src/pages/Home.jsx
+++ car-rental/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import HeroSlide from "../components/UI/HeroSlide";
+import Helmet from "../components/Helmet/Helmet";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Helmet title="Home">
+      <section className="o-0 hero__slider-section">
+        <HeroSlide />
+      </section>
+    </Helmet>
+  );
+};
 
-export default Home
+export default Home;
