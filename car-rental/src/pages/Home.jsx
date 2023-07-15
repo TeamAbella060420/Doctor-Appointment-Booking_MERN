dc71cc5eec4afc7e4ddea752d8172903e5c67bdf
TeamAbella060420 <teamabella@outlook.com>
2023-07-15 12:22:19 +0800

    Update Home.jsx

--- car-rental/src/pages/Home.jsx
+++ car-rental/src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import HeroSlide from "../components/UI/HeroSlide";
 import Helmet from "../components/Helmet/Helmet";
+import { Container, Col, Row } from "reactstrap";
+import FindCarForm from "../components/UI/FindCarForm";
 
 const Home = () => {
   return (
     <Helmet title="Home">
+      {/* ==================== Hero Section ======================= */}
       <section className="hero__slider-section">
         <HeroSlide />
+
+        <div className="hero__form">
+          <Container>
+            <Row className="form__row"> 
+              <Col lg='4' md='4'>
+                <div className="find__cars-left">
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+
+              <Col lg='8' md='8' sm='12'>
+                <FindCarForm />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
       </section>
+
+      {/* ==================== Hero Section ======================= */}
+
     </Helmet>
   );
 };
