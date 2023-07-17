b1d89e4146f8b891ab2e54fa0e122c1566f04e9e
TeamAbella060420 <teamabella@outlook.com>
2023-07-17 16:48:30 +0800

    Update ServicesList.jsx

--- car-rental/src/components/UI/ServicesList.jsx
+++ car-rental/src/components/UI/ServicesList.jsx
@@ -9,4 +9,12 @@ const ServicesList = () => {
   )
 }
 
+const ServiceItem = ({item}) => {
+    <Col lg='4' md='4' sm='6'>
+        <div className="service__item">
+            
+        </div>
+    </Col>
+}
+
 export default ServicesList
