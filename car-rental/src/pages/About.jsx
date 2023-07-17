b37bf81b22301190c1b47bd077d6238492962b9b
TeamAbella060420 <teamabella@outlook.com>
2023-07-17 13:14:17 +0800

    Update About.jsx

--- car-rental/src/pages/About.jsx
+++ car-rental/src/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Helmet from "../components/Helmet/Helmet";
+import AboutSection from '../components/UI/AboutSection';
 
 const About = () => {
   return (
     <Helmet title='About'>
-      About
+      <AboutSection />
     </Helmet>
 
   )
