999880b89b08c3847a6e31577a8d506fbfeceb8c
TeamAbella060420 <teamabella@outlook.com>
2023-07-17 16:39:18 +0800

    Create ServicesList.jsx

---
+++ car-rental/src/components/UI/ServicesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Col } from 'reactstrap'
+import '../../styles/services-list.css'
+import servicesData from '../../assets/data/serviceData'
+
+const ServicesList = () => {
+  return (
+    <div>ServicesList</div>
+  )
+}
+
+export default ServicesList
