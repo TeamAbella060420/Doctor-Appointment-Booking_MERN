d77391d9483caf6acb981604b38ff5259ae6a0f3
TeamAbella060420 <teamabella@outlook.com>
2023-07-18 17:03:36 +0800

    Update BlogList.jsx

--- car-rental/src/components/UI/BlogList.jsx
+++ car-rental/src/components/UI/BlogList.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "../../styles/blog-item.css";
-import { Col, Container, Row } from "reactstrap";
+import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import blogData from "../../assets/data/blogData";
 
-const BlogList = (item) => {
+const BlogList = () => {
   return (
-    <BlogItem data={item}/>
+    <>
+    {
+        blogData.map((item) => (
+            <BlogItem data={item} key={item.id}/>
+        ))
+    }
+    </>
   );
 };
 
-const BlogItem = ({ props }) => {
-  const { imgUrl, title, author, date, time, description, quote } = props.data;
+const BlogItem = ({ data }) => {
+  const { imgUrl, title, author, date, time, description } = data;
   return (
     <Col lg="4" md="4" sm="6">
       <div className="blog__item">
@@ -25,7 +32,19 @@ const BlogItem = ({ props }) => {
 
           <Link to={`/blogs/${title}`}>Read More</Link>
 
-          <div className="blog__item pt-3 mt-3 d-flex align-items-center justify-content-between"></div>
+          <div className="blog__item pt-3 mt-3 d-flex align-items-center justify-content-between">
+            <span className="blog__author">
+                <i className="ri-user-line"></i> {author}
+            </span>
+            <div className="d-flex align-item-center gap-3">
+                <span className="d-flex align-items-center gap-1 section__description">
+                    <i className="ri-calendar-line"></i> {date}
+                </span>
+                <span className="d-flex align-items-center gap-1 section__description">
+                    <i className="ri-time-line"></i> {time}
+                </span>
+            </div>
+          </div>
         </div>
       </div>
     </Col>
