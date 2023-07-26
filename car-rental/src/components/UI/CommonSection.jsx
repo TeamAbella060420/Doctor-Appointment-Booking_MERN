fcbef3dff3e8ed8dd365197cd28772df0bd088ef
TeamAbella060420 <teamabella@outlook.com>
2023-07-26 12:05:17 +0800

    Update CommonSection.jsx

--- car-rental/src/components/UI/CommonSection.jsx
+++ car-rental/src/components/UI/CommonSection.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Container } from 'reactstrap'
+import React from "react";
+import { Container } from "reactstrap";
+import "../../styles/common-section.css";
 
-const CommonSection = (title) => {
+const CommonSection = ({ data }) => {
   return (
-    <section className="common__section">
-        <Container className='text-center'>
-            <h1 className='text-light'>{title}</h1>
-        </Container>
+    <section className="common__section mb-5">
+      <Container className="text-center">
+        <h1 className="text-light">{data}</h1>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default CommonSection
+export default CommonSection;
