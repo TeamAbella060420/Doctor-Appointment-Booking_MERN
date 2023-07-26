931f436204e5ddb33d1b67b627a342852c989a47
TeamAbella060420 <teamabella@outlook.com>
2023-07-26 12:05:25 +0800

    Update Blog.jsx

--- car-rental/src/pages/Blog.jsx
+++ car-rental/src/pages/Blog.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Row } from "reactstrap";
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import BlogList from "../components/UI/BlogList";
 
 const Blog = () => {
   return (
     <div>
-      Blog
+      <Helmet title='Blog'>
+        <CommonSection data='Blog'/>
+        <section className="mb-4">
+          <Container>
+            <Row>
+              <Col lg="12" className="mb-5 text-center">
+                <h6 className="section__subtitle">Explore our blogs</h6>
+                <h2 className="section__title">Latest Blogs</h2>
+              </Col>
+              <BlogList />
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
