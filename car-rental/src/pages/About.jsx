3fbf18b4ad5a3e05b8befac214c4cb21715ba924
TeamAbella060420 <teamabella@outlook.com>
2023-07-26 12:05:23 +0800

    Update About.jsx

--- car-rental/src/pages/About.jsx
+++ car-rental/src/pages/About.jsx
@@ -1,14 +1,81 @@
-import React from 'react'
+import React from "react";
 import Helmet from "../components/Helmet/Helmet";
-import AboutSection from '../components/UI/AboutSection';
+import CommonSection from "../components/UI/CommonSection";
+import AboutSection from "../components/UI/AboutSection";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+import { Col, Container, Row } from "reactstrap";
+import driveImg from "../assets/all-images/drive.jpg";
+import OurMembers from "../components/UI/OurMembers";
 
 const About = () => {
   return (
-    <Helmet title='About'>
-      <dir>About</dir>
+    <Helmet title="About">
+      <CommonSection data="About Us" />
+      <AboutSection />
+      <div className="about__page-section">
+        <Container>
+          <Row className="mb-5">
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section__title">
+                  We are Committed To Provide Safe Ride Solution
+                </h2>
+                <p className="section__description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ducimus magni autem vero nihil veritatis explicabo dolor,
+                  repudiandae perferendis veniam vitae praesentium. Tempora
+                  assumenda sed, dolore sequi veniam ullam inventore eius?
+                </p>
+
+                <p className="section__description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ducimus magni autem vero nihil veritatis explicabo dolor,
+                  repudiandae perferendis veniam vitae praesentium. Tempora
+                  assumenda sed, dolore sequi veniam ullam inventore eius?
+                </p>
+
+                <div className="d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section__subtitle"> Need Any Help?</h6>
+                    <h4>+00123456789</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
+      </div>
+      <BecomeDriverSection />
+
+        <section>
+          <Container>
+            <Row>
+              <Col lg='12' className="mt-5 mb-5 text-center">
+                <h6 className="section__subtitle">
+                  Experts
+                </h6>
+                <h2 className="section__title">
+                  Our Members
+                </h2>
+              </Col>
+                <OurMembers />
+            </Row>
+          </Container>
+        </section>
+        
     </Helmet>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
