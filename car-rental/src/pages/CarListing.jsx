ea7bea703bede8e88dfb5764c390330faf599dd7
TeamAbella060420 <teamabella@outlook.com>
2023-07-26 12:05:27 +0800

    Update CarListing.jsx

--- car-rental/src/pages/CarListing.jsx
+++ car-rental/src/pages/CarListing.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Helmet from "../components/Helmet/Helmet";
+import CommonSection from '../components/UI/CommonSection';
+import CarItem from '../components/UI/CarItem';
+import carData from "../assets/data/carData";
+import { Container, Col, Row } from "reactstrap";
 
 const CarListing = () => {
   return (
     <Helmet title='Carlisting'>
-      Carlisting
+      <CommonSection data='Carlisting'/>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h6 className="section__subtitle">Come with</h6>
+              <h2 className="section__title">Hot Offers</h2>
+            </Col>
+            {carData.slice(0, 6).map((item) => (
+              <CarItem data={item} key={item.id} />
+            ))}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
 
   )
