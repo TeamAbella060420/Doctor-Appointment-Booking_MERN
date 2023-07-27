e8b99418713d79813b2d740b573ac875bc3d9d54
TeamAbella060420 <teamabella@outlook.com>
2023-07-27 12:19:29 +0800

    Update CarDetails.jsx

--- car-rental/src/pages/CarDetails.jsx
+++ car-rental/src/pages/CarDetails.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import carData from "../assets/data/carData";
+import { Container, Col, Row } from "reactstrap";
+import { useParams } from "react-router-dom";
 
 const CarDetails = () => {
-  return (
-    <div>CarDetails</div>
-  )
-}
+  const { slug } = useParams();
 
-export default CarDetails
+  const singleCarItem = carData.find((item) => item.carName === slug);
+
+  return (
+    <Helmet title={singleCarItem.carName}>
+      <section>
+        <Container>
+          <Row className="m-5">
+            <Col lg="6">
+              <img src={singleCarItem.imgUrl} alt="" className="w-100" />
+            </Col>
+            <Col lg="6">
+              <div className="car__info">
+                <h2 className="section__title">{singleCarItem.carName}</h2>
+
+                <div className="d-flex align-items-center gap-5">
+                  <h6 className="rent__price fw-bold fs-4 mt-2 mb-2">
+                    ${singleCarItem.price}.00 / Day
+                  </h6>
+                  <span className="d-flex align-items-center gap-2">
+                    <span style={{ color: "#f9a826" }}>
+                      <i class="ri-star-s-fill"></i>
+                      <i class="ri-star-s-fill"></i>
+                      <i class="ri-star-s-fill"></i>
+                      <i class="ri-star-s-fill"></i>
+                      <i class="ri-star-s-fill"></i>
+                    </span>
+                    ( {singleCarItem.rating} ratings )
+                  </span>
+                </div>
+
+                <p className="section__description">
+                  {singleCarItem.description}
+                </p>
+
+                <div
+                  className="d-flex align-items-center mt-3"
+                  style={{ columnGap: "4rem" }}
+                >
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-roadster-line" style={{ color: "#f9a826" }}></i> {singleCarItem.model}
+                  </span>
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-settings-2-line" style={{ color: "#f9a826" }}></i> {singleCarItem.automatic}
+                  </span>
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-timer-flash-line" style={{ color: "#f9a826" }}></i>
+                    {singleCarItem.speed}
+                  </span>
+                </div>
+
+                <div
+                  className="d-flex align-items-center mt-3"
+                  style={{ columnGap: "2.8rem" }}
+                >
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-map-pin-line" style={{ color: "#f9a826" }}></i> {singleCarItem.gps}
+                  </span>
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-wheelchair-line" style={{ color: "#f9a826" }}></i> {singleCarItem.seatType}
+                  </span>
+                  <span className="d-flex align-items-center gap-1 section__description">
+                    <i class="ri-building-2-line" style={{ color: "#f9a826" }}></i>
+                    {singleCarItem.brand}
+                  </span>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg='7' className="mt-5">
+              <div className="booking-info mt-5">
+                <h4 className="mb-4">
+                  Booking Information
+                </h4>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default CarDetails;
