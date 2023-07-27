2072b9500e774033f6ca5e9614d64064bb067671
TeamAbella060420 <teamabella@outlook.com>
2023-07-27 12:47:35 +0800

    Update BookingForm.jsx

--- car-rental/src/components/UI/BookingForm.jsx
+++ car-rental/src/components/UI/BookingForm.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import "../../styles/booking-form.css";
+import { Form, FormGroup } from "reactstrap";
 
 const BookingForm = () => {
   return (
-    <div>BookingForm</div>
-  )
-}
+    <Form>
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="text" placeholder="First Name" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="Last Name" />
+      </FormGroup>
 
-export default BookingForm
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="number" placeholder="Phone Number" />
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="To Address" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="To Address" />
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <select name="" id="">
+          <option value="1 person">1 Person</option>
+          <option value="2 person">2 Person</option>
+          <option value="3 person">3 Person</option>
+          <option value="4 person">4 Person</option>
+          <option value="5+ person">5+ Person</option>
+        </select>
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <select name="" id="">
+          <option value="1 luggage">1 Luggage</option>
+          <option value="2 luggage">2 Luggage</option>
+          <option value="3 luggage">3 Luggage</option>
+          <option value="4 luggage">4 Luggage</option>
+          <option value="5+ luggage">5+ Luggage</option>
+        </select>
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="To Address" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="To Address" />
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default BookingForm;
