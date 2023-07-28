717603d1ffb2b65af57173046b03a7c8a31eb25f
TeamAbella060420 <teamabella@outlook.com>
2023-07-28 16:52:39 +0800

    Update BlogDetails.jsx

--- car-rental/src/pages/BlogDetails.jsx
+++ car-rental/src/pages/BlogDetails.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+import { useParams } from 'react-router-dom';
+import blogData from '../assets/data/blogData';
 
 const BlogDetails = () => {
   return (
