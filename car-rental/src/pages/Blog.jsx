c4065239fb3399e331e9bd7724e2dee272333295
TeamAbella060420 <teamabella@outlook.com>
2023-07-28 16:52:37 +0800

    Update Blog.jsx

--- car-rental/src/pages/Blog.jsx
+++ car-rental/src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Col, Row } from "reactstrap";
+import { Container, Row } from "reactstrap";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import BlogList from "../components/UI/BlogList";
@@ -7,16 +7,15 @@ import BlogList from "../components/UI/BlogList";
 const Blog = () => {
   return (
     <div>
-      <Helmet title='Blog'>
-        <CommonSection data='Blog'/>
+      <Helmet title='Blogs'>
+        <CommonSection data='Blogs'/>
         <section className="mb-4">
           <Container>
             <Row>
-              <Col lg="12" className="mb-5 text-center">
-                <h6 className="section__subtitle">Explore our blogs</h6>
-                <h2 className="section__title">Latest Blogs</h2>
-              </Col>
               <BlogList />
+            </Row>
+            <Row className="mt-3">
+            <BlogList />
             </Row>
           </Container>
         </section>
