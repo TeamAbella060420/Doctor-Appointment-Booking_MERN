acdc79caafbe7f9c8bc34bd28583690abdbbac63
TeamAbella060420 <teamabella@outlook.com>
2023-07-28 17:24:40 +0800

    Update BlogDetails.jsx

--- car-rental/src/pages/BlogDetails.jsx
+++ car-rental/src/pages/BlogDetails.jsx
@@ -1,37 +1,38 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
-import { useParams } from 'react-router-dom';
-import blogData from '../assets/data/blogData';
-import Helmet from '../components/Helmet/Helmet';
+import { useParams } from "react-router-dom";
+import blogData from "../assets/data/blogData";
+import Helmet from "../components/Helmet/Helmet";
 
 const BlogDetails = () => {
-
   const { slug } = useParams();
-  
+
   const singleBlogItem = blogData.find((item) => item.title === slug);
-  
+
   // Function to scroll to the top of the component
   const scrollToTop = () => {
-    window.scrollTo({ top: 0,});
+    window.scrollTo({ top: 0 });
   };
 
-    // Call the scrollToTop function when the component mounts
-    useEffect(() => {
-      scrollToTop();
-    }, []);
+  // Call the scrollToTop function when the component mounts
+  useEffect(() => {
+    scrollToTop();
+  }, [singleBlogItem]);
   return (
-    <Helmet title={singleBlogItem}>
+    <Helmet title={singleBlogItem.title}>
       <section>
         <Container>
-          <Row className='m-5'>
-            <Col lg='6'>
-              <img src={singleBlogItem.img} alt="" />
+          <Row>
+            <Col lg="8" md="8">
+              <div className="blog__details">
+                <img src={singleBlogItem.img} alt="" />
+              </div>
             </Col>
           </Row>
         </Container>
       </section>
     </Helmet>
-  )
-}
+  );
+};
 
-export default BlogDetails
+export default BlogDetails;
